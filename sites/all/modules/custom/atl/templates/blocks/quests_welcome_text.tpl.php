d588a0e3ee793674552a2ec2c1cf5485e208db97
Joseph Cheek <joseph.cheek@turner.com>
2017-06-11 19:10:49 -0400

    text changes

--- sites/all/modules/custom/atl/templates/blocks/quests_welcome_text.tpl.php
+++ sites/all/modules/custom/atl/templates/blocks/quests_welcome_text.tpl.php
@@ -1,6 +1,6 @@
 <div class="welcome">
   <div class="elder-image point">
   </div>
-  <p class="quote">Find some place to hide!</p>
-  <p>You hear him yell this at you as you run away.&nbsp; Where can you go?</p>
+  <p class="quote">Wake up!&nbsp; Wake up!</p>
+  <p>You suddenly jolt awake.&nbsp; What is happening?</p>
 </div>
