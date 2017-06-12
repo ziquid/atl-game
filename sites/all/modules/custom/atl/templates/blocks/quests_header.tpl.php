537644323339789ae99cdce1900b59339cc27062
Joseph Cheek <joseph.cheek@turner.com>
2017-06-11 22:35:27 -0400

    separate row from quest-header

--- sites/all/modules/custom/atl/templates/blocks/quests_header.tpl.php
+++ sites/all/modules/custom/atl/templates/blocks/quests_header.tpl.php
@@ -1,24 +1,26 @@
-<div class="quest-header row">
-  <div class="left col-xs-1">
-    <?php if ($qgo): ?>
-      <a href="/<?php echo $qgo; ?>">
-        <span class="arrows bigbig">&lsaquo;&lsaquo;&lsaquo;</span>
-      </a>
-    <?php endif; ?>
-  </div>
-  <div class="middle col-xs-10">
-    <h1 class="chapter">
-      <!--Chapter <?php print $groupToShow; ?>: --><?php echo $qg; ?>
-    </h1>
-    <div class="location">
-      <?php print $questLocation; ?>
-    </div>
-  </div>
-  <div class="right col-xs-1">
-    <?php if (isset($qgn)): ?>
-        <a href="/<?php echo $qgn; ?>">
-            <span class="arrows bigbig">&rsaquo;&rsaquo;&rsaquo;</span>
+<div class="row">
+  <div class="quest-header">
+    <div class="left col-xs-1">
+      <?php if ($qgo): ?>
+        <a href="/<?php echo $qgo; ?>">
+          <span class="arrows bigbig">&lsaquo;&lsaquo;&lsaquo;</span>
         </a>
-    <?php endif; ?>
+      <?php endif; ?>
+    </div>
+    <div class="middle col-xs-10">
+      <h1 class="chapter">
+        <!--Chapter <?php print $groupToShow; ?>: --><?php echo $qg; ?>
+      </h1>
+      <div class="location">
+        <?php print $questLocation; ?>
+      </div>
+    </div>
+    <div class="right col-xs-1">
+      <?php if (isset($qgn)): ?>
+          <a href="/<?php echo $qgn; ?>">
+              <span class="arrows bigbig">&rsaquo;&rsaquo;&rsaquo;</span>
+          </a>
+      <?php endif; ?>
+    </div>
   </div>
 </div>
