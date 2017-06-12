068b31a3f0694908072fc47d1885dcbb4caefc54
Joseph Cheek <joseph.cheek@turner.com>
2017-06-11 22:46:07 -0400

    take more rows outside the text

--- sites/all/modules/custom/atl/templates/blocks/quest_group_completion.tpl.php
+++ sites/all/modules/custom/atl/templates/blocks/quest_group_completion.tpl.php
@@ -1,7 +1,9 @@
-<div class="quest-group-completion row">
-  <strong><?php echo $questGroup->completed; ?></strong> of <?php echo $questGroup->total; ?>
-  <?php ge('@quest'); ?>s complete
-  <?php if ($questGroup->timesCompleted > 0): ?>
-    (2nd round)
-  <?php endif; ?>
+<div class="row">
+  <div class="quest-group-completion">
+    <strong><?php echo $questGroup->completed; ?></strong> of <?php echo $questGroup->total; ?>
+    <?php ge('@quest'); ?>s complete
+    <?php if ($questGroup->timesCompleted > 0): ?>
+      (2nd round)
+    <?php endif; ?>
+  </div>
 </div>
