cca2b00935a0fcc38b05b165a8e1cb27b89b3960
Joseph Cheek <joseph.cheek@turner.com>
2017-07-10 21:08:46 -0400

    tooltips for welcome screen

--- sites/all/modules/custom/atl/templates/blocks/button.tpl.php
+++ sites/all/modules/custom/atl/templates/blocks/button.tpl.php
@@ -2,14 +2,23 @@
 
 $game = check_plain(arg(0));
 $arg2 = check_plain(arg(2));
-$link = $game . '/' . $link . '/' . $arg2 . $extra_link;
+if (strlen($link)) {
+  $link = $game . '/' . $link . '/' . $arg2 . $extra_link;
+}
+if (!strlen($text)) {
+  $text = $type;
+}
 
 ?>
 <button class="game-button-exterior center-block">
   <span class="game-button-interior game-button-<?php echo drupal_html_class($type); ?>">
-    <a href="/<?php echo $link;?>">
-      <?php echo $type; ?> &raquo;
-    </a>
+    <?php if (strlen($link)): ?>
+      <a href="/<?php echo $link;?>">
+    <?php endif; ?>
+    <?php echo $text; ?>
+    <?php if (strlen($link)): ?>
+      </a>
+    <?php endif; ?>
   </span>
   <?php if ($subhead): ?>
     <p>
