a9100711c96c2c52e4d46046371a3a7e238e0b11
Joseph Cheek <joseph.cheek@turner.com>
2017-07-11 20:55:07 -0400

    restyling the header

--- sites/all/modules/custom/atl/templates/blocks/header.tpl.php
+++ sites/all/modules/custom/atl/templates/blocks/header.tpl.php
@@ -1,55 +1,75 @@
 <div class="game-header game-hood-<?php echo $game_user->fkey_neighborhoods_id; ?>">
   <div class="pull-left">
-    <div class="money">
-      <span id="money-id">
-        <?php echo $moneyStr; ?>
-      </span>
-      <span class="money-text">
-        <?php echo $game_user->values_name; ?>
-      </span>
+
+    <div class="header-item level">
+      <div class="progress">
+        <div class="progress-bar" role="progressbar"
+             aria-valuenow="<?php echo $experience_pct; ?>"
+             aria-valuemin="0" aria-valuemax="100"
+             style="width:<?php echo $experience_pct; ?>%">
+          <span id="level-id">
+            <?php echo $experience_pct; ?>%
+          </span>
+        </div>
+      </div>
+      <strong><?php echo $game_user->level; ?></strong>
+    </div>
+
+    <div class="header-item money">
+      <div class="progress">
+        <div class="progress-bar" role="progressbar"
+             aria-valuenow="<?php echo $game_user->money; ?>"
+             aria-valuemin="0" aria-valuemax="100"
+             style="width:100%">
+          <span id="money-id">
+            <?php echo $moneyStr; ?>
+          </span>
+        </div>
+      </div>
+      <i class="fa fa-btc" aria-hidden="true">
+      </i>
       <div id="income-time">
         <?php echo $incomeText; ?>
       </div>
     </div>
-    <div class="actions">
-      <span id="actions-id">
-        <?php echo $game_user->actions; ?>
-      </span>
-      /
-      <?php echo $game_user->actions_max; ?>
-      <span class="actions-text">
-        <?php ge('@actions'); ?>
-      </span>
-      <br>
+
+    <div class="header-item actions">
+      <div class="progress">
+        <div class="progress-bar" role="progressbar"
+             aria-valuenow="<?php echo $game_user->actions; ?>"
+             aria-valuemin="0" aria-valuemax="100"
+             style="width:<?php echo $game_user->actions * 100 / $game_user->actions_max; ?>%">
+          <span id="actions-id">
+            <?php echo $game_user->actions; ?>
+          </span>&nbsp;/&nbsp;<?php echo $game_user->actions_max; ?>
+        </div>
+      </div>
+      <i class="fa fa-at" aria-hidden="true">
+      </i>
       <div id="actions-time">
         <?php echo $actionsText; ?>
       </div>
     </div>
-    <div class="experience">
-      <strong><?php echo $game_user->experience; ?></strong>
-      <?php echo $experience_pct; ?>
-      <div class="experience-text">
-        Experience
+
+    <div class="header-item energy">
+      <div class="progress">
+        <div class="progress-bar" role="progressbar"
+          aria-valuenow="<?php echo $game_user->energy; ?>"
+          aria-valuemin="0" aria-valuemax="100"
+          style="width:<?php echo $game_user->energy * 100 / $game_user->energy_max; ?>%">
+          <span id="energy-id">
+            <?php echo $game_user->energy; ?>
+          </span>&nbsp;/&nbsp;<?php echo $game_user->energy_max; ?>
+        </div>
       </div>
-    </div>
-    <div class="energy">
-      <span id="energy-id">
-        <?php echo $game_user->energy; ?>
-      </span>
-      /
-      <?php echo $game_user->energy_max; ?>
-      <span class="energy-text">
-        <?php ge('@energy'); ?>
-      </span>
-      <br>
+      <i class="fa fa-bolt" aria-hidden="true">
+      </i>
       <div id="energy-time">
         <?php echo $energyText; ?>
       </div>
     </div>
-    <div class="level">
-      <?php echo $level_text; ?>
-    </div>
   </div>
+
   <div class="home pull-right">
     <a href="/<?php echo $game; ?>/home/<?php echo $arg2; ?>">
       <!--<img src="/sites/default/files/images/<?php echo $game; ?>_home_icon_large.png"/>-->
